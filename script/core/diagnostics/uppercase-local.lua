45e215a89075133db4dc6a688299debc0689ff7a
Andrea Walchshofer <andrea.walchshofer@scch.at>
2023-02-14 14:24:22 +0100

    Implement basic diagnostic

---
+++ script/core/diagnostics/uppercase-local.lua
@@ -0,0 +1,52 @@
+local files     = require 'files'
+local guide     = require 'parser.guide'
+local lang      = require 'language'
+local config    = require 'config'
+local vm        = require 'vm'
+local util      = require 'utility'
+
+local function isDocClass(source)
+    if not source.bindDocs then
+        return false
+    end
+    for _, doc in ipairs(source.bindDocs) do
+        if doc.type == 'doc.class' then
+            return true
+        end
+    end
+    return false
+end
+
+-- If local elements must be named lowercase by coding convention, this diagnostic helps with reminding about that
+return function (uri, callback)
+    local ast = files.getState(uri)
+    if not ast then
+        return
+    end
+
+
+
+    guide.eachSourceType(ast.ast, 'setglobal', function (source)
+        local name = guide.getKeyName(source)
+        if not name then
+            return
+        end
+        local first = name:match '%w' -- alphanumeric character
+        if not first then
+            return
+        end
+        if not first:match '%u' then -- uppercase
+            return
+        end
+        -- If the assignment is marked as doc.class, then it is considered allowed 
+        if isDocClass(source) then
+            return
+        end
+        
+        callback {
+            start   = source.start,
+            finish  = source.finish,
+            message = lang.script.DIAG_UPPERCASE_LOCAL,
+        }
+    end)
+end
