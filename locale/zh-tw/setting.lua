427491d0ca3ead7fc8cc1fdf2dee6f05088151be
Andrea Walchshofer <andrea.walchshofer@scch.at>
2023-02-14 17:19:09 +0100

    Add entries into script and setting to remaining locales

--- locale/zh-tw/setting.lua
+++ locale/zh-tw/setting.lua
@@ -412,8 +412,10 @@ config.diagnostics['unknown-operator']      = -- TODO: need translate!
 'Enable diagnostics for unknown operators.'
 config.diagnostics['unreachable-code']      = -- TODO: need translate!
 'Enable diagnostics for unreachable code.'
-config.diagnostics['global-element']       = -- TODO: need translate!
+config.diagnostics['global-element']        = -- TODO: need translate!
 'Enable diagnostics to warn about global variables.'
+config.diagnostics['uppercase-local']       = -- TODO: need translate!
+'Enable uppercase local variable definition diagnostics.'
 config.typeFormat.config                    = -- TODO: need translate!
 'Configures the formatting behavior while typing Lua code.'
 config.typeFormat.config.auto_complete_end  = -- TODO: need translate!
