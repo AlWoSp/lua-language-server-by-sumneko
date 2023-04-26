59e7647fd420ea9161d25b5bbd2e63ab189c5d2f
AndreaWalchshoferSCCH <andrea.walchshofer@scch.at>
2023-04-26 11:11:25 +0200

    Add basic test

---
+++ test/diagnostics/namestyle-check.lua
@@ -0,0 +1,37 @@
+local config = require 'config'
+local util   = require 'utility'
+
+-- disable all default groups to make isolated tests
+config.set(nil, 'Lua.diagnostics.groupFileStatus', 
+{
+    ['ambiguity']     = 'None',
+    ['await']         = 'None',
+    ['codestyle']     = 'None',
+    ['conventions']   = 'None',
+    ['duplicate']     = 'None',
+    ['global']        = 'None',
+    ['luadoc']        = 'None',
+    ['redefined']     = 'None',
+    ['strict']        = 'None',
+    ['strong']        = 'None',
+    ['type-check']    = 'None',
+    ['unbalanced']    = 'None',
+    ['unused']        = 'None'
+})
+
+-- enable single diagnostic that is to be tested
+config.set(nil, 'Lua.diagnostics.neededFileStatus',
+{
+    ['namestyle-check'] = 'Any!' -- override groupFileStatus
+})
+
+-- check that namestyle-check is executed
+TEST [[
+local <!NAMEnOTmATCHINGcONVENTION!> = "test"
+]]
+
+-- reset configurations
+config.set(nil, 'Lua.diagnostics.groupFileStatus', 
+{})
+config.set(nil, 'Lua.diagnostics.neededFileStatus',
+{})
